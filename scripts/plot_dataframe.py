f08eb435e892d4fc6e66ee719d39f16a11abfb39
kryofly <kryofly@space>
2017-12-21 13:47:08 +0100

    plot buy/sell, also enable backtest to get the profit-data (proft plot to-be-done)

--- scripts/plot_dataframe.py
+++ scripts/plot_dataframe.py
@@ -12,6 +12,8 @@ from freqtrade import exchange, analyze
 from freqtrade.dataframe import load_dataframe
 from freqtrade.strategy import Strategy
 from freqtrade.misc import parse_args_common
+from freqtrade import optimize
+from freqtrade.optimize.backtesting import backtest
 
 # we could reuse misc.py:parse_args() if split up and composable
 def plot_parse_args(args: List[str]):
@@ -23,17 +25,32 @@ def plot_parse_args(args: List[str]):
         default = 'BTC_XLM',
         type = str,
     )
+    parser.add_argument(
+        '-b', '--backtest',
+        help = 'BACKTEST=yes -- Run backtest, plot buy/sell occurencies',
+        default = False,
+        type=bool,
+    )
     return parser.parse_args(args)
 
-def plot_analyzed_dataframe(strategy: Strategy, pair: str) -> None:
+def plot_analyzed_dataframe(args, strategy: Strategy, pairs: str) -> None:
     """
     Calls analyze() and plots the returned dataframe
     :param pair: pair as str
     :return: None
     """
 
-    ld = load_dataframe(ticker_interval=5, pairs=pair)
-    d = ld[pair[0]];
+    backtest_result = None
+
+    if args.backtest:
+        print('---- calling backtest ----')
+        data = optimize.load_data(ticker_interval=strategy.tick_interval(), pairs=pairs)
+        prepdata = optimize.preprocess(strategy, data)
+        backtest_results = backtest(strategy, prepdata, 1, True)
+        print('---- backtesting done ----')
+
+    ld = load_dataframe(ticker_interval=5, pairs=pairs)
+    d = ld[pairs[0]];
     dataframe = analyze.analyze_ticker(strategy,d)
 
     dataframe.loc[dataframe['buy']  == 1, 'buy_price']  = dataframe['close']
@@ -41,18 +58,24 @@ def plot_analyzed_dataframe(strategy: Strategy, pair: str) -> None:
 
     # Two subplots sharing x axis
     fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
-    fig.suptitle(pair, fontsize=14, fontweight='bold')
+    fig.suptitle(pairs, fontsize=14, fontweight='bold')
     ax1.plot(dataframe.index.values, dataframe['close'], label='close')
-    # ax1.plot(dataframe.index.values, dataframe['sell'], 'ro', label='sell')
+    #ax1.plot(dataframe.index.values, dataframe['sell'], 'ro', label='sell')
     #ax1.plot(dataframe.index.values, dataframe['sma'], '--', label='SMA')
     #ax1.plot(dataframe.index.values, dataframe['tema'], ':', label='TEMA')
     #ax1.plot(dataframe.index.values, dataframe['blower'], '-.', label='BB low')
     #ax1.plot(dataframe.index.values, dataframe['buy_price'], 'bo', label='buy')
     ax1.legend()
 
-    #ax2.plot(dataframe.index.values, dataframe['adx'], label='ADX')
-    #ax2.plot(dataframe.index.values, dataframe['mfi'], label='MFI')
-    # ax2.plot(dataframe.index.values, [25] * len(dataframe.index.values))
+    # take the backtest_result and:
+    # pick out the buy-sell pairs and make an accumulated profit graph
+    # write that graph into dataframe['profit'] for example
+    if backtest_result:
+        None
+
+    # plot the buy/sell occurencies
+    ax2.plot(dataframe.index.values, dataframe['buy'], 'bo', label='Buy')
+    ax2.plot(dataframe.index.values, dataframe['sell'], 'ro', label='Sell')
     ax2.legend()
 
     ax3.plot(dataframe.index.values, dataframe['fastk'], label='k')
@@ -71,5 +94,5 @@ if __name__ == '__main__':
     args = plot_parse_args(sys.argv[1:])
     print('---- Plotting currency pair:', args.pair)
     strategy = Strategy().load(args.strategy)
-    plot_analyzed_dataframe(strategy, [args.pair])
+    plot_analyzed_dataframe(args, strategy, [args.pair])
 
