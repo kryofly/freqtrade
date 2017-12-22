ca87d841a0219c71a50a57c5e3fc7d00a121dc9c
kryofly <kryofly@space>
2017-12-22 22:56:07 +0100

    enable sell signals and cleanup logic
    FIX20171222: test needed, if we disable sell-signals tests still passes

--- freqtrade/trade.py
+++ freqtrade/trade.py
@@ -49,6 +49,7 @@ def min_roi_reached(strategy: Strategy, trade,
 
 # Make this a pure function, that only returns True/False,
 # Depending on wheter to exit this trade
+# Returns True if we should exit this trade
 def handle_trade(strategy: Strategy, trade) -> bool:
     """
     Sells the current pair if the threshold is reached and updates the trade record.
@@ -67,11 +68,11 @@ def handle_trade(strategy: Strategy, trade) -> bool:
     # we need to call trade session flush here
 
     # Check if minimal roi has been reached
-    if not min_roi_reached(strategy, trade, current_rate, datetime.utcnow()):
-        return False
-
+    if min_roi_reached(strategy, trade, current_rate, datetime.utcnow()):
+        return True
+    # FIX20171222: test needed, if we disable sell-signals tests still passes
     #logger.debug('Checking sell_signal ...')
-    #if not get_signal(strategy, trade.pair, SignalType.SELL):
-    #    return False
+    if get_signal(strategy, trade.pair, SignalType.SELL):
+        return True
 
-    return True
+    return False
